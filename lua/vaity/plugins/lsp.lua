9d0798ad8b040002816270b40a64bd30d8f753f5
KamiNuke <150977125+KamiNuke@users.noreply.github.com>
2024-09-13 09:23:40 +0300

    added cmp

--- lua/vaity/plugins/lsp.lua
+++ lua/vaity/plugins/lsp.lua
@@ -4,8 +4,23 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
     },
     config = function()
+        local cmp = require("cmp")
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities()
+        )
+
+
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = { "clangd" },
@@ -16,7 +31,9 @@ return {
             -- and will be called for each installed server that doesn't have
             -- a dedicated handler.
             function (server_name) -- default handler (optional)
-                require("lspconfig")[server_name].setup {}
+                require("lspconfig")[server_name].setup {
+                    capabilities = capabilities
+                }
             end,
             -- Next, you can provide a dedicated handler for specific servers.
             -- For example, a handler override for the `rust_analyzer`
@@ -77,6 +94,14 @@ return {
     end,
   },
 }
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ['<C-e>'] = cmp.mapping.confirm({ select = true }),
+            }),
 
+            sources = cmp.config.sources({
+                { name = "nvim_lsp"},
+            }),
+        })
     end
 }
